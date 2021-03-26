3eb385e685246361e95cdd5c61b8483a42e00494
Kevin Useche <nivek4511@gmail.com>
2021-03-26 18:17:12 -0500

    exportar a excel

--- js/main.js
+++ js/main.js
@@ -65,6 +65,12 @@ document.querySelector('#sell').addEventListener('click', ()=> {
     }
 });
 
+/* export */
+document.querySelector('#export').addEventListener('click', (e)=> {
+    e.preventDefault();
+    exportTableToExcel(table,'registros');
+});
+
 
 function updateStock(){
     let record;
@@ -88,5 +94,35 @@ function updateStock(){
     });
 }
 
-updateStock(); 
+function exportTableToExcel(tableID, filename = ''){
+    var downloadLink;
+    var dataType = 'application/vnd.ms-excel';
+    var tableSelect = tableID;
+    var tableHTML = tableSelect.outerHTML.replace(/ /g, '%20');
+    
+    // Specify file name
+    filename = filename?filename+'.xls':'excel_data.xls';
+    
+    // Create download link element
+    downloadLink = document.createElement("a");
+    
+    document.body.appendChild(downloadLink);
+    
+    if(navigator.msSaveOrOpenBlob){
+        var blob = new Blob(['ufeff', tableHTML], {
+            type: dataType
+        });
+        navigator.msSaveOrOpenBlob( blob, filename);
+    }else{
+        // Create a link to the file
+        downloadLink.href = 'data:' + dataType + ', ' + tableHTML;
+    
+        // Setting the file name
+        downloadLink.download = filename;
+        
+        //triggering the function
+        downloadLink.click();
+    }
+}
 
+updateStock(); 
